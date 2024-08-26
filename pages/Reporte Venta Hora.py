583db2a3ec2a7d6feebd1878cb8590da0466ac8c
rorocabezasjis <117833337+rorocabezasjis@users.noreply.github.com>
2024-08-26 18:15:27 -0400

    Update Reporte Venta Hora.py

--- pages/Reporte Venta Hora.py
+++ pages/Reporte Venta Hora.py
@@ -9,7 +9,7 @@ import plotly.express as px
 
 
 # Configuración de la conexión a la base de datos utilizando SQLAlchemy
-engine = create_engine('mysql+pymysql://jysparki_jis:Jis2020!@103.72.78.28/jysparki_jis') 
+engine = create_engine('mysql+pymysql://jysparki_admin:Admin2024$!@216.137.190.82/jysparki_jis')
 
 st.header("Reporte de Venta por Hora") 
 
