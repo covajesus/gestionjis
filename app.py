2046ce61b6ed4329e4584b0fd27f5d1c90af228d
rorocabezasjis <117833337+rorocabezasjis@users.noreply.github.com>
2024-08-26 18:17:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 # Configuración de la conexión a la base de datos utilizando SQLAlchemy
-engine = create_engine('mysql+pymysql://jysparki_jis:Jis2020!@103.72.78.28/jysparki_jis') 
+engine = create_engine('mysql+pymysql://jysparki_admin:Admin2024$!@216.137.190.82/jysparki_jis')
 
 # Configuración de la conexión a la base de datos
 connection = pymysql.connect(
